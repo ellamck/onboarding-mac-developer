652fdf096fc31c66cda02f61db7331d0fab376af
Ella <emckercher@student.unimelb.edu.au>
2026-08-26 13:34:55 +1000

    add guard and if let chaining to playground file

--- EllaMcKercher/SwiftBasics/Variables Constants and Data Types.playground/Contents.swift
+++ EllaMcKercher/SwiftBasics/Variables Constants and Data Types.playground/Contents.swift
@@ -22,3 +22,29 @@ var studentIdDict: [Int: String] = [123: "Ella", 234: "Blake", 345: "Harry"]
 for (id, name) in studentIdDict {
     print("Student Name: \(name) | Student ID: \(id)")
 }
+
+// Guard and If-Let Chaining
+
+// Using an if-let for unwrapping an optional
+func greetStudent(firstName: String, middleName: String?, lastName: String) {
+    if let middle = middleName {
+        print("Hello \(firstName) \(middle) \(lastName) nice to meet you!")
+        return
+    } else {
+        print("Hello \(firstName) \(lastName) nice to meet you!")
+    }
+}
+
+greetStudent(firstName: "Ella", middleName: nil, lastName: "McKercher")
+greetStudent(firstName: "Ella", middleName: "Rose", lastName: "McKercher")
+
+// Using a guard chaining
+func checkStudentId(id: Int?, studentName: String) {
+    guard let studentID = id else {
+        print("All students must have an ID.")
+        return
+    }
+    print("Welcome to university \(studentName) your student ID number is \(studentID)")
+}
+
+checkStudentId(id: nil, studentName: "Ella")
